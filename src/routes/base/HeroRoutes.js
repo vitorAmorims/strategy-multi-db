678d054e67684545a3c63b560018987dfa1978f9
vitorAmorims <vtamorims@gmail.com>
2021-08-09 10:49:53 -0300

    validacoes query com joi

--- src/routes/base/HeroRoutes.js
+++ src/routes/base/HeroRoutes.js
@@ -1,4 +1,10 @@
+const Joi = require('joi');
 const BaseRoute = require('./BaseRoute')
+
+const failAction = (request, h, erro) => {
+    throw erro
+}
+
 class HeroRoutes extends BaseRoute{
     constructor(db){
         super()
@@ -8,20 +14,34 @@ class HeroRoutes extends BaseRoute{
         return {
             method: 'GET',
             path: '/herois',
-            handler: (request, h) => {
+            config: {
+                validate: {
+                    failAction: failAction,
+                    query: {
+                        skip: Joi.number().integer().default(0),
+                        limit: Joi.number().integer().default(10),
+                        nome: Joi.string().min(2).max(100)
+                    }
+                }
+            },
+            handler: (request, h, err) => {
                 try{
-                    const { nome, skip = 0, limit = 10 } = request.query
+                    const { nome, skip, limit} = request.query
                     // console.log('skip', typeof(skip))
                     // console.log('limit', limit)
                     // console.log('nome',nome)
 
                     let query = {}
-                    if (nome) query.nome = nome
-                    if (isNaN(skip))
-                        throw Error('skip must be positive number')
-                    if (isNaN(limit))
-                        throw Error('limit must be positive number')
-                    return this.db.read(query, parseInt(skip), parseInt(limit))
+                    // if (nome) query.nome = nome
+                    // if (isNaN(skip))
+                    //     throw Error('skip must be positive number')
+                    // if (isNaN(limit))
+                    //     throw Error('limit must be positive number')
+                    query = { 
+                        nome: {$regex: `.*${nome}*.`}
+                    }
+
+                    return this.db.read(nome ? query : {}, skip, limit)
                 }catch(error){
                     console.log('Algum erro', error);
                     return 'Erro interno no servidor'
--- src/tests/apiHeroes.test.js
+++ src/tests/apiHeroes.test.js
@@ -1,67 +1,77 @@
-const assert = require('assert');
-const api = require('../api')
-let app = {}
+const assert = require("assert");
+const api = require("../api");
+let app = {};
 
-describe('Suite de teste da api Heroes', function () {
-    this.beforeAll(async () => {
-        app = await api
-    })
-    it('listar /herois', async () => {
-        const result = await app.inject({
-            method: 'GET',
-            url: '/herois?skip=0&limit=10'
-        })
+describe("Suite de teste da api Heroes", function () {
+  this.beforeAll(async () => {
+    app = await api;
+  });
+  it("listar /herois", async () => {
+    const result = await app.inject({
+      method: "GET",
+      url: "/herois?skip=0&limit=10",
+    });
 
-        const data = JSON.parse(result.payload)
-        const statusCode = result.statusCode
+    const data = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
 
-        assert.deepEqual(statusCode, 200)
-        assert.ok(Array.isArray(data));
-    })
-    it('listar /herois - deve retornar somente 3 registros', async () => {
-        const TAMANHO_LIMITE = 3
-        const result = await app.inject({
-            method: 'GET',
-            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
-        })
-        const data = JSON.parse(result.payload)
+    assert.deepEqual(statusCode, 200);
+    assert.ok(Array.isArray(data));
+  });
+  it("listar /herois - deve retornar somente 3 registros", async () => {
+    const TAMANHO_LIMITE = 3;
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`,
+    });
+    const data = JSON.parse(result.payload);
 
-        const statusCode = result.statusCode
+    const statusCode = result.statusCode;
 
-        assert.deepEqual(statusCode, 200)
-        assert.ok(data.length === TAMANHO_LIMITE);
-    })
-    it('listar /herois - deve retornar erro ao inserir skip ou limit isNAN', async () => {
-        const TAMANHO_LIMITE = 'aaaaaaa'
-        const result = await app.inject({
-            method: 'GET',
-            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
-        })
+    assert.deepEqual(statusCode, 200);
+    assert.ok(data.length === TAMANHO_LIMITE);
+  });
+  it("listar /herois - deve retornar erro ao inserir skip ou limit isNAN", async () => {
+    const TAMANHO_LIMITE = "aaaaaaa";
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`,
+    });
 
-        const msg = result.payload
-        
-        assert.deepEqual(msg, 'Erro interno no servidor')
-    })
-    it('listar /herois - deve pesquisar uma nome nos primeiros 1000 registros', async () => {
-        const TAMANHO_LIMITE = 1000
-        let nome;
-        const result = await app.inject({
-            method: 'GET',
-            url: `/herois`
-        })
-        
-        const data = JSON.parse(result.payload)
-        nome = data[0].nome
+    const objErroMsg = result.payload;
 
-        const result1 = await app.inject({
-            method: 'GET',
-            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${nome}`
-        })
+    const errorResult = {
+      statusCode: 400,
+      error: "Bad Request",
+      message: 'child "limit" fails because ["limit" must be a number]',
+      validation: {
+        source: "query",
+        keys: ["limit"],
+      },
+    };
 
-        const objectData = JSON.parse(result1.payload)
-        const statusCode = result1.statusCode
+    assert.deepStrictEqual(objErroMsg, JSON.stringify(errorResult));
+  });
+  it("listar /herois - deve pesquisar uma nome nos primeiros 1000 registros", async () => {
+    const TAMANHO_LIMITE = 1000;
+    let nome;
+    const result = await app.inject({
+      method: "GET",
+      url: `/herois`,
+    });
 
-        assert.deepStrictEqual(statusCode, 200)
-        assert.ok(nome === objectData[0].nome)
-    })
-})
+    const data = JSON.parse(result.payload);
+    nome = data[0].nome;
+
+    const result1 = await app.inject({
+      method: "GET",
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${nome}`,
+    });
+
+    const objectData = JSON.parse(result1.payload);
+    const statusCode = result1.statusCode;
+
+    assert.deepStrictEqual(statusCode, 200);
+    assert.ok(nome === objectData[0].nome);
+  });
+});
